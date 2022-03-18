57de33f8aea977e88fc8f7de8f2a68eebbb9fa00
Calvin Chung <calvin-chung@live.com>
2022-03-17 17:17:13 -0700

    revised with test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,22 @@
 const request = require('request');
-const args = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  if (error) {
-    console.log("Error!:", error);
-  }
-  let data =  JSON.parse(body);
-  console.log(data[0].description);
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+  
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    let data =  JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback("Breed not found", "Breed not found");
+    }
+    
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
